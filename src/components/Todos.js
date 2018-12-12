828bac9ba976e87cc2c39333ec853823f09c2578
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-12-12 21:37:02 +0200

    Feat: display todo list

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react'
 
-class Todos extends Component {
-    render() {
-        return(
-            <div>
-                <p>Todo's List</p>
-            </div>
-        );
-    }
+const Todos = ({todos}) => {
+        
+    //check todos state length todos.length > 0 ?
+    //if true display todos list
+    //if false display a message 'There are no todos!'
+    const TodoList = todos.length > 0 ?
+            ( 
+            todos.map(todo => {
+                return(
+                    <div id={todo.id}>
+                        {todo.task}
+                    </div>
+                )
+                
+            })
+            
+        ) : (
+            <div>There are no todos!</div>
+        )
+    
+    return(
+        <div>
+            <h3>Todo's List</h3>
+            {TodoList}
+        </div>
+    )
+    
 }
 
 export default Todos;
