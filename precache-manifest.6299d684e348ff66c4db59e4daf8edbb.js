aa818bc97b92827387866800ea87a40c61d795f5
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-12-16 20:35:45 +0200

    Updates

---
+++ precache-manifest.6299d684e348ff66c4db59e4daf8edbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "84c007b1203ce2fd8a61",
+    "url": "/todo-app/static/js/runtime~main.84c007b1.js"
+  },
+  {
+    "revision": "d502913623c7475e7b03",
+    "url": "/todo-app/static/js/main.d5029136.chunk.js"
+  },
+  {
+    "revision": "263d29d520cdd6137791",
+    "url": "/todo-app/static/js/1.263d29d5.chunk.js"
+  },
+  {
+    "revision": "d502913623c7475e7b03",
+    "url": "/todo-app/static/css/main.91fe984d.chunk.css"
+  },
+  {
+    "revision": "d81ae5ec42db6a4d584ff9f0828aabe8",
+    "url": "/todo-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/todo-app/precache-manifest.f89213a4331451a32dda343613e9a461.js"
+  "/todo-app/precache-manifest.6299d684e348ff66c4db59e4daf8edbb.js"
 );
 
 workbox.clientsClaim();
