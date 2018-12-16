849c377d45957713da08bd92a82c5b38b5081940
Sarah Hassan Ahmed <sarah27h88@yahoo.com>
2018-12-16 17:41:13 +0200

    Updates

---
+++ precache-manifest.eb4ba47e0729c2ccd04192f7d45940f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "84c007b1203ce2fd8a61",
+    "url": "/todo-app/static/js/runtime~main.84c007b1.js"
+  },
+  {
+    "revision": "8c58b136a8b19c5852f0",
+    "url": "/todo-app/static/js/main.8c58b136.chunk.js"
+  },
+  {
+    "revision": "a03b901df2caec10d050",
+    "url": "/todo-app/static/js/1.a03b901d.chunk.js"
+  },
+  {
+    "revision": "8c58b136a8b19c5852f0",
+    "url": "/todo-app/static/css/main.ce1061f1.chunk.css"
+  },
+  {
+    "revision": "8e1b2e2968af9d56a1e23af4a02504ed",
+    "url": "/todo-app/index.html"
+  }
+];
